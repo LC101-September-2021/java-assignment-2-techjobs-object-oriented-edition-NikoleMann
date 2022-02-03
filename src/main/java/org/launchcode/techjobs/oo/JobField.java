c808bddedb78b71b7c3553a795838bba8692e6c6
NikoleMann <Nikole.d.mann@gmail.com>
2022-02-02 20:07:44 -0600

    Finished!

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,40 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+
+     int id;
+     static int nextId = 1;
+     String value;
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
+        if (this == o) return true;
+        if (!(o instanceof Employer)) return false;
+        Employer employer = (Employer) o;
+        return getId() == employer.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
